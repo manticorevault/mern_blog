29d5a8a0d36a811c8b97b6a77d10d64203778327
manticorevault <artur_seilah@hotmail.com>
2020-08-30 19:06:57 +0100

    Added the Layout component to the frontend

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fe0f53f856330177e4f9";
+/******/ 	var hotCurrentHash = "3e5087ac63b5b33fcc91";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
