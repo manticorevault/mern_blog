a1fb067c3cf4a5613bf0fc174946ff4954b6e58b
manticorevault <artur_seilah@hotmail.com>
2020-09-05 00:14:35 +0100

    Added the Register Form

--- frontend/components/auth/RegisterComponent.js
+++ frontend/components/auth/RegisterComponent.js
@@ -1,6 +1,21 @@
 const RegisterComponent = () => {
+    const registerForm = () => {
+        return (
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <input 
+                        type="text" 
+                        className="form-control"
+                        placeholder="Insert Your Name">
+
+                    </input>
+                </div>
+            </form>
+        )
+    }
+
     return (
-        <h2> Register </h2>
+        { registerForm }
     )
 }
 
