81f99e7bf96519a9fb3803d1e43f84b4e4267040
manticorevault <artur_seilah@hotmail.com>
2020-09-06 00:28:00 +0100

    Added the input in the register form

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "484bb85c331ec5af4668";
+/******/ 	var hotCurrentHash = "c0924ba81e95cbf386a7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/components/auth/RegisterComponent.js
+++ frontend/components/auth/RegisterComponent.js
@@ -1,9 +1,20 @@
 const RegisterComponent = () => {
+    const handleSubmit = () => {
+        e.preventDefault()
+        console.log("Submit handled")
+    }
+
+    const handleChange = (e) => {
+        event.preventDefault()
+        console.log(e.target.value)
+    }
+
     const registerForm = () => {
         return (
             <form onSubmit={handleSubmit}>
                 <div className="form-group">
                     <input 
+                        onChange={ handleChange }
                         type="text" 
                         className="form-control"
                         placeholder="Insert Your Name">
@@ -15,7 +26,9 @@ const RegisterComponent = () => {
     }
 
     return (
-        { registerForm }
+        <React.Fragment>
+            { registerForm() }
+        </React.Fragment>
     )
 }
 
