25a7023a02a0bd0905530973fe82fc2c9f70de01
manticorevault <artur_seilah@hotmail.com>
2020-09-30 16:51:36 +0100

    Fixed once more the blog.js controller, still getting the same errors

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0d67d34c49987e2241b1";
+/******/ 	var hotCurrentHash = "640f1c02bbe44d190884";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
