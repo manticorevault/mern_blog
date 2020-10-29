5a38b47a126065cf26d3e3c720aa31a5bf2e4734
manticorevault <artur_seilah@hotmail.com>
2020-10-29 14:08:35 +0000

    Added intial styles to the Single Page. Displaying the blog.body in the Single Page as well as the Cover Picture

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "191ad572cb0ad72864b1";
+/******/ 	var hotCurrentHash = "356890d098736b0f593c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/pages/blogs/[slug].js
+++ frontend/pages/blogs/[slug].js
@@ -1,24 +1,91 @@
 import Head from "next/head";
 import Link from "next/link";
-import { withRouter } from "next/router";
 import Layout from "../../components/Layout"
 import { useState } from "react";
+import renderHTML from "react-render-html"
+import moment from "moment"
 import { singleBlog } from "../../actions/blog";
 import { API, DOMAIN, APP_NAME } from "../../config"
 
-const SingleBlog = ({ blog, router }) => {
+const SingleBlog = ({ blog }) => {
+
+    const showCategories = blog =>
+
+        blog.categories.map((category, index) => (
+            <Link key={index} href={`/categories/${category.slug}`}>
+                <a className="btn btn-outline-primary mr-1 ml-1 mt-3">
+                    {category.name}
+                </a>
+            </Link>
+        ))
+
+    const showTags = blog =>
+
+        blog.tags.map((tag, index) => (
+            <Link key={index} href={`/tags/${tag.slug}`}>
+                <a className="btn btn-outline-info mr-1 ml-1 mt-3">
+                    {tag.name}
+                </a>
+            </Link>
+        ))
+
     return (
         <React.Fragment>
             <Layout>
                 <main>
                     <article>
                         <div className="container-fluid">
-                            <section>{JSON.stringify(blog)}</section>
+                            <section>
+                                <div className="row" style={{ marginTop: "-30px" }}>
+
+                                    <img src={`${API}/blog/photo/${blog.slug}`} alt={blog.title} className="img img-fluid featured-image" />
+
+                                </div>
+                            </section>
+
+                            <section>
+                                <p className="lead mt-3 mark">
+                                    Escrito por {blog.postedBy.name} | Publicado {moment(blog.updatedAt).fromNow()}
+                                </p>
+
+                                <div className="pb-3">
+
+                                    {showCategories(blog)}
+                                    {showTags(blog)}
+                                    <br />
+                                    <br />
+
+                                </div>
+                            </section>
                         </div>
+
+                        <div className="container pb-5">
+                            <section>
+                                <div className="col-md-12 lead">
+                                    {renderHTML(blog.body)}
+                                </div>
+                            </section>
+                        </div>
+
+
+                        <div className="container pb-5">
+                            <h4 className="text-center pt-5 pb-5 h2">
+                                Voce Tambem Pode Gostar
+                            </h4>
+
+                            <hr />
+
+                            <p> Mostrar Mais Relacionados</p>
+                        </div>
+
+                        <div className="container pb-5">
+                            <p> Mostrar Comentarios</p>
+                        </div>
+
                     </article>
                 </main>
             </Layout>
-        </React.Fragment>
+        </React.Fragment >
     );
 };
 
@@ -32,4 +99,4 @@ SingleBlog.getInitialProps = ({ query }) => {
     });
 };
 
-export default withRouter(SingleBlog);
+export default SingleBlog;
