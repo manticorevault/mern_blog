b879cb0afc8c3d1dba5633e526b5a8220f7543f9
manticorevault <artur_seilah@hotmail.com>
2020-11-02 12:54:26 +0000

    Added latest builds from the development build

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2c4414db0e5ac68fb3b9";
+/******/ 	var hotCurrentHash = "605bc037fe1e118e8d04";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
